d3cd2cd448d60781c106932756341edb2178e35a
Lailson Bandeira <lailson@guava.com.br>
2011-06-06 10:30:07 -0300

    Removing demos temporarily.

--- demos/demo-1-simple/app/models/phone.rb
+++
@@ -1,3 +0,0 @@
-class Phone < ActiveRecord::Base
-  belongs_to :person
-end
--- demos/demo-1-simple/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Demo1Simple::Application.config.secret_token = '4e1f4edcb7d348f8ee72e4da8c5e12de09258d814ff51bba01485bf8766fd3388fc3c2665af3632142e08d500eab1ad5e28c1817eb117d4e32230f42d2329e07'
