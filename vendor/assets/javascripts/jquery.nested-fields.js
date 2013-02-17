c3dde45c5a308e24a3ea5c1e3a311b2047021ccf
Lailson Bandeira <lailsonbm@gmail.com>
2013-02-17 10:22:40 -0300

    Inserting empty element only if it exists.

--- vendor/assets/javascripts/jquery.nested-fields.js
+++ vendor/assets/javascripts/jquery.nested-fields.js
@@ -1,4 +1,6 @@
 (function($) {
+  'use strict';
+
   /**
    *
    * Awesome Nested Fields
@@ -7,7 +9,7 @@
    * https://github.com/lailsonbm/awesome_nested_fields
    *
    */
-   
+
   var defaultSettings = {
     beforeInsert: function(item, callback) { callback() },
     afterInsert: function(item) {},
@@ -23,11 +25,11 @@
     newItemIndex: 'new_nested_item',
     unescapeTemplate: true
   };
-  
+
   // PUBLIC API
   var methods = {
     init: function(options) {
-      return this.each(function() {        
+      return this.each(function() {
         var $this = $(this);
         if($this.data('nested-fields.options')) {
           log('Nested fields already defined for this element. If you want to redefine options, destroy it and init again.');
@@ -39,40 +41,40 @@
         options.emptyTemplate = $(options.emptyTemplateSelector, $this);
         options.container = $(options.containerSelector, $this);
         options.add = $(options.addSelector, $this);
-        $this.data('nested-fields.options', options); 
+        $this.data('nested-fields.options', options);
 
         bindInsertToAdd(options);
         bindRemoveToItems(options, $this);
       });
     },
-    
+
     insert: function(callback, options) {
       options = $.extend({}, getOptions(this), options);
       return insertItemWithCallbacks(callback, options);
     },
-    
+
     remove: function(element, options) {
       options = $.extend({}, getOptions(this), options);
       return removeItemWithCallbacks(element, options);
     },
-    
+
     removeAll: function(options) {
       options = $.extend({}, getOptions(this), options);
       $(methods.items.apply(this)).each(function(i, el) {
         methods.remove(el, options);
       });
     },
-    
+
     items: function() {
       return findItems(getOptions(this));
     },
-    
+
     destroy: function() {
       $(this).removeData('nested-fields.options');
       $('*', this).unbind('.nested-fields');
     }
   };
-  
+
   $.fn.nestedFields = function(method) {
     if (methods[method]) {
       return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
@@ -82,9 +84,9 @@
       $.error('Method ' +  method + ' does not exist on jQuery.nestedFields');
     }
   };
-  
+
   // Initialization functions
-  
+
   function getOptions(element) {
     var $element = $(element);
     while($element.length > 0) {
@@ -97,26 +99,26 @@
     }
     return null;
   }
-  
+
   function bindInsertToAdd(options) {
     options.add.bind('click.nested-fields', function(e) {
       e.preventDefault();
       insertItemWithCallbacks(null, options);
     });
   }
-  
+
   function bindRemoveToItems(options, $this) {
     $(options.itemSelector, $this).each(function(i, item) {
       bindRemoveToItem(item, options);
     });
   }
-  
+
   // Insertion functions
-  
+
   function prepareTemplate(options) {
     var regexp = new RegExp(options.newItemIndex, 'g');
     var newId = new Date().getTime();
-    
+
     var contents = options.itemTemplate.html();
     if(options['unescapeTemplate']) {
       contents = unescape_html_tags(contents);
@@ -124,15 +126,15 @@
     var newItem = $(contents.replace(regexp, newId));
     newItem.attr('data-new-record', true);
     newItem.attr('data-record-id', newId);
-    
+
     bindRemoveToItem(newItem, options);
-    
+
     return newItem;
   }
-  
-  function insertItemWithCallbacks(onInsertCallback, options) {  
+
+  function insertItemWithCallbacks(onInsertCallback, options) {
     var newItem = prepareTemplate(options);
-    
+
     function insert() {
       if(onInsertCallback) {
         onInsertCallback(newItem);
@@ -140,8 +142,8 @@
       removeEmpty(options);
       options.container.append(newItem);
     }
-    
-    if(!options.skipBefore) {      
+
+    if(!options.skipBefore) {
       options.beforeInsert(newItem, insert);
       if(options.beforeInsert.length <= 1) {
         insert();
@@ -149,20 +151,20 @@
     } else {
       insert();
     }
-    
+
     if(!options.skipAfter) {
       options.afterInsert(newItem);
     }
-    
+
     return newItem;
   }
-  
+
   function removeEmpty(options) {
     findEmpty(options).remove();
   }
-  
+
   // Removal functions
-  
+
   function removeItemWithCallbacks(element, options) {
     function remove() {
       if($element.attr('data-new-record')) { // record is new
@@ -173,7 +175,7 @@
       }
       insertEmpty(options);
     }
-    
+
     var $element = $(element);
     if(!options.skipBefore) {
       options.beforeRemove($element, remove);
@@ -183,28 +185,30 @@
     } else {
       remove();
     }
-    
+
     if(!options.skipAfter) {
       options.afterRemove($element);
     }
-    
+
     return $element;
   }
-  
+
   function insertEmpty(options) {
     if(findItems(options).length === 0) {
       var contents = options.emptyTemplate.html();
-      if(options['unescapeTemplate']) {
-        contents = unescape_html_tags(contents);
+      if(contents) {
+        if(options['unescapeTemplate']) {
+          contents = unescape_html_tags(contents);
+        }
+        options.container.append(contents);
       }
-      options.container.append(contents);
     }
   }
-  
+
   function bindRemoveToItem(item, options) {
     var removeHandler = $(item).find(options.removeSelector);
     var needsConfirmation = removeHandler.attr('data-confirm');
-    
+
     var event = needsConfirmation ? 'confirm:complete' : 'click';
     removeHandler.bind(event + '.nested-fields', function(e, confirmed) {
       e.preventDefault();
@@ -214,29 +218,29 @@
       return false;
     });
   }
-  
+
   // Find functions
-  
+
   function findItems(options) {
     return options.container.find(options.itemSelector + ':visible');
   }
-  
+
   function findEmpty(options) {
     return options.container.find(options.emptySelector);
   }
-  
+
   // Utility functions
-  
+
   function unescape_html_tags(html) {
     var e = document.createElement('div');
     e.innerHTML = html;
     return e.childNodes.length === 0 ? "" : jQuery.trim(e.childNodes[0].nodeValue);
   }
-  
+
   function log(msg) {
     if(console && console.log) {
       console.log(msg);
     }
   }
-  
+
 })(jQuery);
